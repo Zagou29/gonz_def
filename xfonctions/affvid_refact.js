b9438a0f9f3f6de71c78b1df0afaba12054f2396
Gonzague de Poulpiquet <gdepoulpiquet@me.com>
2026-02-20 16:19:09 +0100

    enlevé le console.log

--- xfonctions/affvid_refact.js
+++ xfonctions/affvid_refact.js
@@ -171,7 +171,6 @@ export class Affvid {
     // Activer la lecture automatique
     if (iframe.src) {
       iframe.src = iframe.src.replace("autoplay=0", "autoplay=1");
-      console.log(iframe.src);
     }
 
     this.#container.append(videoElement);
