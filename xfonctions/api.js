5e381d318b357d8f35bf99af207fedcc604a589d
Gonzague de Poulpiquet <gdepoulpiquet@me.com>
2026-03-01 13:59:15 +0100

    api.js ne sert plus

--- xfonctions/api.js
+++
@@ -1,6 +0,0 @@
-export const fetchJSON = async (url, options = {}) => {
-  const headers = { Accept: "application/json", ...options.headers };
-  const r = await fetch(url, { ...options, headers });
-  if (!r.ok) throw new Error("Erreur serveur", { cause: r });
-  return r.json();
-};
